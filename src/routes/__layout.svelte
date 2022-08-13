27da6e1b8134e37a14e7de9e4322e61d297162b9
resqiar <resqiageng@gmail.com>
2022-08-13 19:43:45 +0700

    style: add progress bar when navigating around

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,30 @@
-<script>
-	import '../app.css';
+<script lang="ts">
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	import '../styles/app.css';
+	import '../styles/nprogress.css';
+
+	/**
+	 * NProgress Configs
+	 * Simply show loading progress at the top
+	 * When navigating around client-side
+	 * @see https://github.com/rstacruz/nprogress#configuration
+	 */
+	NProgress.configure({
+		showSpinner: false,
+		speed: 500,
+		minimum: 0.16
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <slot />
