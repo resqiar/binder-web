649a468346549bc98aeca61d3a40d70612e6fa31
resqiar <resqiageng@gmail.com>
2022-08-14 20:35:03 +0700

    feat: add github repo and semver

--- src/components/header/IndexHeader.svelte
+++ src/components/header/IndexHeader.svelte
@@ -1,5 +1,8 @@
 <script>
+	import { WEB_SEMVER } from '../../data/version';
+
 	import Logo from '../brand/Logo.svelte';
+	import GithubIcon from '../icons/GithubIcon.svelte';
 </script>
 
 <div class="navbar bg-base-100 md:px-8">
@@ -8,7 +11,7 @@
 		<label
 			for="left-drawer"
 			aria-label="Open Drawer"
-			class="btn btn-ghost btn-square drawer-button hidden md:flex"
+			class="btn btn-square btn-ghost drawer-button hidden md:flex"
 		>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -31,19 +34,34 @@
 	</div>
 
 	<div class="flex-none">
-		<button class="btn btn-ghost btn-square">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				class="inline-block h-5 w-5 stroke-current"
-				><path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-				/></svg
+		<div class="dropdown-end dropdown">
+			<label tabindex="0" for="dropdown-menu" class="btn btn-ghost btn-circle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block h-5 w-5 stroke-current"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+					/></svg
+				>
+			</label>
+			<ul
+				id="dropdown-menu"
+				tabindex="0"
+				class="dropdown-content menu rounded-box w-52 bg-base-100 shadow-2xl"
 			>
-		</button>
+				<li>
+					<a href="https://github.com/resqiar/binder" target="_blank">
+						<GithubIcon />
+						GitHub</a
+					>
+				</li>
+				<li><p>Web: v{WEB_SEMVER}</p></li>
+			</ul>
+		</div>
 	</div>
 </div>
---
+++ src/data/version.ts
@@ -0,0 +1,1 @@
+export const WEB_SEMVER = '1.0.0';
