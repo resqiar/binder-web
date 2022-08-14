9328f7e089aad1b3d1bf1f78bbf60123827f3598
resqiar <resqiageng@gmail.com>
2022-08-14 13:26:08 +0700

    feat: enable edge functions for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,10 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter()
+    adapter: vercel({
+      // enable edge function for vercel 
+      edge: true,
+    })
   }
 };
 
