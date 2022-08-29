e064655d2ceb37dbcd53daf565e5d06b6a44e0eb
resqiar <resqiageng@gmail.com>
2022-08-29 23:48:57 +0700

    feat: show qr code for faster switch

--- src/components/body/DetailBody.svelte
+++ src/components/body/DetailBody.svelte
@@ -3,8 +3,23 @@
 	export let data: IExtDetail;
 </script>
 
-<main class="flex items-center justify-center">
-	<div class="card mx-2 my-4 w-full bg-base-100 shadow-2xl lg:mt-10 lg:mb-8 lg:w-96">
+<main class="flex flex-col-reverse items-center justify-center lg:flex-row lg:gap-12">
+	<div class="card mb-8 h-min w-full bg-base-100 shadow-2xl lg:mt-10 lg:w-[200px]">
+		<div class="card-body flex items-center justify-center">
+			<img
+				src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&ecc=H&data=${data.id}`}
+				alt={data.title}
+				width="150"
+				height="150"
+			/>
+			<div class="mt-4 flex flex-col items-center justify-center">
+				<h2 class="card-title text-center">Scan me</h2>
+				<p class="text-center text-sm">Open Binder app and scan the QR code</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="card mx-2 my-4 w-full bg-base-100 shadow-2xl lg:mt-10 lg:w-96">
 		{#if data.image_url}
 			<figure>
 				<img
