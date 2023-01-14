36fe50f15cd05a82168fb611e6d4b78dbba70c7f
resqiar <resqiageng@gmail.com>
2023-01-14 20:59:20 +0700

    feat: add caching strategy for getBatch of exts endpoint

--- src/lib/getBatch.ts
+++ src/lib/getBatch.ts
@@ -3,7 +3,13 @@ import type { IExtension } from '../types/extension';
 export async function getBatch(skip: number = 0) {
 	try {
 		const url = `${import.meta.env.VITE_SERVER}/ext?skip=${skip}`;
-		const response = await fetch(url);
+
+		const response = await fetch(url, {
+			cache: 'default',
+			headers: {
+				'cache-control': 'public, max-age=3600' // Free to cache for max age of 1 hour
+			}
+		});
 
 		if (!response.ok) {
 			throw new Error(`Request failed with the status code ${response.status}`);
